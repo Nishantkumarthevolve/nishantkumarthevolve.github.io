e19093a18a7379e157750a13cc3b865fd56c892c
Nishantkumarthevolve <nishantkumar19952301@gmail.com>
2021-02-10 15:30:44 +0530

    Updates

---
+++ main.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("helo, world"),window.addEventListener("scroll",(function(o){var l=document.querySelector(".header-container");console.log(window.scrollY),0!==window.scrollY?l.classList.add("head-scroll"):l.classList.remove("head-scroll")}))})();
+//# sourceMappingURL=main.js.map
